640b91c8345438a8ddd6ef5b39cd20699f2f1273
Sam Parsons <samparsons269@gmail.com>
2021-06-25 19:00:48 -0400

    refactor main file to use workshopper-adventure/exercise modules

--- demo-workshopper.js
+++ demo-workshopper.js
@@ -1,17 +1,26 @@
 #!/usr/bin/env node
 
-const workshopper = require('workshopper'),
+const workshopper = require('workshopper-adventure'),
   path = require('path');
 
 function fpath(f) {
   return path.join(__dirname, f);
 }
 
-workshopper({
+const shop = workshopper({
   name: 'demo-workshopper',
   title: 'Demo Workshopper',
   subtitle: 'Learn how to create a workshopper lesson',
   appDir: __dirname,
-  menuItems: [],
   exerciseDir: fpath('./exercises/'),
+  header: require('workshopper-adventure/default/header'),
+  footer: require('workshopper-adventure/default/footer'),
+  fail: require('workshopper-adventure/default/fail'),
+  pass: require('workshopper-adventure/default/pass'),
 });
+
+require('./exercises/menu.json').forEach((name) => {
+  shop.add(name);
+});
+
+module.exports = shop;
